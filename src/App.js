b6e200de617f47353d0f32741d3bd327c3d54667
Phuong Nguyen <nie.mp296@gmail.com>
2020-01-10 21:18:32 -0800

    starting template

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Mental Health App!
+        </p>
+        <a
+          className="App-link"
+          href="https://docs.google.com/presentation/d/1_AsFBV1M9crXbdV_lFcrrF4NZ-UPIoSl97fIgwuxy94/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click for Details!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
