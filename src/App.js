16655e58439afa36c449392f5709bcf5dd6ac3a0
Parker Landsman <plandsma@calpoly.edu>
2020-01-29 18:37:17 -0800

    Newest upload

--- src/App.js
+++ src/App.js
@@ -5,44 +5,80 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Resources
-        </p>
-      </header>
-      <body>
-      <div className="App-Resource">
-              <header className="Resource">
-                  <p>
-                  Campus Resources
-                  </p>
-              </header>
-              <div className="Title">
-                              <header className="Title1">
-                                  <p>
-                                  Title1
-                                  </p>
-                               </header>
-                             </div>
-        </div>
-
-        <div className="App-Resource">
-          <header className="Resource">
-             <p>
-             Local Resources
-             </p>
-           </header>
+       <body>
+          <div className="Big-Box">
+             <header className="Resource-Overall">
+                <h1>Resources</h1>
+                <p>
+                 Mental health issues are challenging to deal with alone.
+                 </p>
+                 <p>
+                 Just know that we are here for you, please reach out!
+                 </p>
+            </header>
          </div>
-
-        <div className="App-Resource">
+        <div className="SmallBoxOutline">
+         <div className="Small-Box">
           <header className="Resource">
+             <h1>Resources</h1>
              <p>
-             National Resources
-             </p>
+              Resource 1
+              </p>
           </header>
          </div>
+         <div className="Small-Box">
+             <header className="Resource">
+                <h1>Resources</h1>
+                <p>
+                 Resource 2
+                 </p>
+             </header>
+        </div>
+        <div className="Small-Box">
+            <header className="Resource">
+               <h1>Resources</h1>
+               <p>
+                Resource 3
+                </p>
+            </header>
+           </div>
+        <div className="Small-Box">
+        <header className="Resource">
+           <h1>Resources</h1>
+           <p>
+            Resource 3
+            </p>
+        </header>
+         </div>
+         <div className="Small-Box">
+                 <header className="Resource">
+                    <h1>Resources</h1>
+                    <p>
+                     Resource 3
+                     </p>
+                 </header>
+                  </div>
+          <div className="Small-Box">
+                  <header className="Resource">
+                     <h1>Resources</h1>
+                     <p>
+                      Resource 3
+                      </p>
+                  </header>
+                   </div>
+          <div className="Small-Box">
+                  <header className="Resource">
+                     <h1>Resources</h1>
+                     <p>
+                      Resource 3
+                      </p>
+                  </header>
+                   </div>
+        </div>
       </body>
     </div>
+
+
 
 
 
